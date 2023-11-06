5abde70ee77bcd341a03b49e7e46cbd41550c2c8
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-11-06 19:30:40 +0000

    DEMO-6 Added 'thankyou' code

---
+++ thankyou.py
@@ -0,0 +1,6 @@
+
+def thankyou(word):
+    """ Returns concatenated 'Thank You ' and 'word' in a string. """
+    print("Thank You " + word)
+
+thankyou("world")
--- world.py
+++
@@ -1,7 +0,0 @@
-
-def hello(word):
-    """ Returns concatenated 'Hello' and 'word' in a string. """
-    print("hello " + word)
-
-hello("world")
-
